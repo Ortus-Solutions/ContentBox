b435910ed917757e271d35790765f7c9510410ad
Luis Majano <lmajano@gmail.com>
2014-06-19 19:22:45 -0700

    wrong paths

--- workbench/create-patch.sh
+++ workbench/create-patch.sh
@@ -35,10 +35,10 @@ echo "Start Commit: $1"
 echo "End Commit: $2"
 
 # create and tar up a release
-#git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | xargs tar -rf $SCRIPTDIR/patches/$3/patch.tar
+#git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | xargs tar -rf $4/workbench/patches/$3/patch.tar
 
 # create and zip up a patch release
-git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | zip $SCRIPTDIR/patches/$3/patch.zip -@
+git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | zip $4/workbench/patches/$3/patch.zip -@
 
 # create delete filelisting patch, exclude external stuff
-git diff-tree -r --name-only --no-commit-id --diff-filter=D $1 $2 | grep -E -v '^(includes|config|tests?|views|model|layouts|handlers|workbench)' > $SCRIPTDIR/patches/$3/deletes.txt
+git diff-tree -r --name-only --no-commit-id --diff-filter=D $1 $2 | grep -E -v '^(includes|config|tests?|views|model|layouts|handlers|workbench)' > $4/workbench/patches/$3/deletes.txt
