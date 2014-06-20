34d63740efcccbf383b106fa54fceb0cf4018f71
Luis Majano <lmajano@gmail.com>
2014-06-19 20:40:29 -0700

    updated for correct repo paths

--- workbench/create-patch.sh
+++ workbench/create-patch.sh
@@ -1,44 +1,52 @@
 #!/bin/sh
-echo Creating ContentBox Release Updates Patch
 
-# check params
+# check start commit id
 if [ -z "$1" ]
 then
 	echo "First commit ID is not defined"
 	echo "Usage:$0 startCommitHash endCommitHash ContentBoxVersion"
 	exit
 fi
+
+# check end commit id
 if [ -z "$2" ]
 then
 	echo "End commit ID is not defined"
-	echo "Usage:$0 startCommitHash endCommitHash ContentBoxVersion"
+	echo "Usage:$0 startCommitHash endCommitHash ContentBoxVersion repoRoot"
 	exit
 fi
+
+# check version
 if [ -z "$3" ]
 then
 	echo "ContentBox Release Version is Needed"
-	echo "Usage:$0 startCommitHash endCommitHash ContentBoxVersion"
+	echo "Usage:$0 startCommitHash endCommitHash ContentBoxVersion repoRoot"
 	exit
 fi
+
+# check repository root
 if [ -z "$4" ]
 then
-	# default to script dir
-	SCRIPTDIR=`pwd`
-	# go back to root for diff of project
-	cd ..
+	echo "ContentBox Repsoitory Root is Needed"
+	echo "Usage:$0 startCommitHash endCommitHash ContentBoxVersion repoRoot"
+	exit
 else
-	cd $4
+	REPO_ROOT=$4
 fi
 
-echo "Running commands from: " `pwd`
-echo "Start Commit: $1"
-echo "End Commit: $2"
+echo ****************************************************************************
+echo Start Commit: $1
+echo End Commit: $2
+echo Version: $3
+echo Repo Root: $4
+echo ****************************************************************************
+echo 
 
-# create and tar up a release
-#git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | xargs tar -rf $4/workbench/patches/$3/patch.tar
+# switch to repo root
+cd $REPO_ROOT
 
 # create and zip up a patch release
-git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | zip $4/workbench/patches/$3/patch.zip -@
+git diff-tree -r --name-only --no-commit-id --diff-filter=ACMRT $1 $2 | zip $REPO_ROOT/workbench/patches/$3/patch.zip -@
 
 # create delete filelisting patch, exclude external stuff
-git diff-tree -r --name-only --no-commit-id --diff-filter=D $1 $2 | grep -E -v '^(includes|config|tests?|views|model|layouts|handlers|workbench)' > $4/workbench/patches/$3/deletes.txt
+git diff-tree -r --name-only --no-commit-id --diff-filter=D $1 $2 | grep -E -v '^(includes|config|tests?|views|model|layouts|handlers|workbench)' > $REPO_ROOT/workbench/patches/$3/deletes.txt
