298b3bbfb6749c690ae2fe7c4f465aa39e981e00
Jon Clausen <jon_clausen@silowebworks.com>
2022-06-17 13:44:29 -0400

    Fix indentation formatting

--- modules/contentbox/modules/contentbox-admin/modules/contentbox-ckeditor/elixir-module.js
+++ modules/contentbox/modules/contentbox-admin/modules/contentbox-ckeditor/elixir-module.js
@@ -3,29 +3,29 @@ const path = require( "path" );
 
 module.exports = function(mix) {
 
-	var moduleDeps = [
-		"node_modules/ckeditor/plugins",
-		"node_modules/ckeditor/adapters",
-		"node_modules/ckeditor/lang",
-		"node_modules/ckeditor/ckeditor.js",
-		"node_modules/ckeditor/styles.js",
-		"node_modules/ckeditor/contents.css"
-	];
+    var moduleDeps = [
+        "node_modules/ckeditor/plugins",
+        "node_modules/ckeditor/adapters",
+        "node_modules/ckeditor/lang",
+        "node_modules/ckeditor/ckeditor.js",
+        "node_modules/ckeditor/styles.js",
+        "node_modules/ckeditor/contents.css"
+    ];
 
-	var destination = "modules/contentbox/modules/contentbox-admin/modules/contentbox-ckeditor/includes/ckeditor";
+    var destination = "modules/contentbox/modules/contentbox-admin/modules/contentbox-ckeditor/includes/ckeditor";
 
-	fs.rmdirSync( destination, { recursive :true } );
+    fs.rmdirSync( destination, { recursive :true } );
 
-	moduleDeps.forEach( src => fs.copySync( src, `${destination}/${src.split( '/' ).pop()}`, { overwrite : true } ) );
+    moduleDeps.forEach( src => fs.copySync( src, `${destination}/${src.split( '/' ).pop()}`, { overwrite : true } ) );
 
-	fs.copySync( "node_modules/ckeditor/skins/moono-lisa", `${destination}/skins/moono-lisa` );
+    fs.copySync( "node_modules/ckeditor/skins/moono-lisa", `${destination}/skins/moono-lisa` );
 
-	var localPluginsDir = "modules/contentbox/modules/contentbox-admin/resources/assets/plugins/ckeditor/plugins";
+    var localPluginsDir = "modules/contentbox/modules/contentbox-admin/resources/assets/plugins/ckeditor/plugins";
 
-	fs.readdirSync(localPluginsDir).forEach( element => {
-		fs.copySync( path.join(localPluginsDir, element ), path.join( destination + '/plugins', element ), { overwrite : true } );
+    fs.readdirSync(localPluginsDir).forEach( element => {
+        fs.copySync( path.join(localPluginsDir, element ), path.join( destination + '/plugins', element ), { overwrite : true } );
     });
 
-	fs.copySync( "modules/contentbox/modules/contentbox-admin/resources/assets/plugins/ckeditor/config.js", path.join( destination, 'config.js' ), { overwrite: true } );
+    fs.copySync( "modules/contentbox/modules/contentbox-admin/resources/assets/plugins/ckeditor/config.js", path.join( destination, 'config.js' ), { overwrite: true } );
 
 };
--- modules/contentbox/modules/contentbox-admin/modules/contentbox-filebrowser/elixir-module.js
+++ modules/contentbox/modules/contentbox-admin/modules/contentbox-filebrowser/elixir-module.js
@@ -1,19 +1,19 @@
 const fs = require('fs-extra');
 
 module.exports = function(mix) {
-	elixir.config.mergeConfig(
-		{
-			plugins: [
-				{
-					apply: (compiler) => compiler.hooks.afterEmit.tap(
-											'AfterEmitPlugin',
-											() => fs.copySync(
-													'node_modules/jquery/dist/jquery.min.js',
-													'modules/contentbox/modules/contentbox-admin/modules/contentbox-filebrowser/includes/js/jquery.min.js'
-												)
-										)
-				}
-			]
-    	}
-	);
+    elixir.config.mergeConfig(
+        {
+            plugins: [
+                {
+                    apply: (compiler) => compiler.hooks.afterEmit.tap(
+                                            'AfterEmitPlugin',
+                                            () => fs.copySync(
+                                                    'node_modules/jquery/dist/jquery.min.js',
+                                                    'modules/contentbox/modules/contentbox-admin/modules/contentbox-filebrowser/includes/js/jquery.min.js'
+                                                )
+                                        )
+                }
+            ]
+        }
+    );
 };
