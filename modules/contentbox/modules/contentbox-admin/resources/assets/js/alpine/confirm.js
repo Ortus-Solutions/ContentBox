877bd6e3bb831b3148491e9b57578bbeac5b7a87
Jon Clausen <jon_clausen@silowebworks.com>
2022-10-29 10:20:17 -0400

    remove dead code

--- modules/contentbox/modules/contentbox-admin/resources/assets/js/alpine/confirm.js
+++ modules/contentbox/modules/contentbox-admin/resources/assets/js/alpine/confirm.js
@@ -1,44 +1,45 @@
-//$confirm
-// import bootstrap from "bootstrap";
+// Magic: $confirm
 
-Alpine.magic('confirm', el => ( message, options ) => {
+Alpine.magic("confirm", (el) => (message, options) => {
 	return new Promise((resolve, reject) => {
 		const modalOptions = {
-			id : "modal-confirm",
-			className : "modal",
-			acceptText : "Accept",
-			cancelText : "Cancel"
-
+			id: "modal-confirm",
+			className: "modal",
+			acceptText: "Accept",
+			cancelText: "Cancel",
 		};
-		if( options ) Object.assign( modalOptions, options );
-		const modalElem = document.createElement('div')
-		modalElem.id = modalOptions.id
-		modalElem.className = modalOptions.className
+		if (options) Object.assign(modalOptions, options);
+		const modalElem = document.createElement("div");
+		modalElem.id = modalOptions.id;
+		modalElem.className = modalOptions.className;
 		modalElem.innerHTML = `
 		  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
 			<div class="modal-content">
 			  <div class="modal-body fs-6">
-				<p>${message || 'Are you sure?'}</p>
+				<p>${message || "Are you sure?"}</p>
 			  </div>    <!-- modal-body -->
 			  <div class="modal-footer" style="border-top:0px">
-			    <button id="modal-btn-cancel" type="button" class="btn btn-secondary">${modalOptions.cancelText}</button>
-			    <button id="modal-btn-accept" type="button" class="btn btn-primary">${modalOptions.acceptText}</button>
+			    <button id="modal-btn-cancel" type="button" class="btn btn-secondary">${
+					modalOptions.cancelText
+				}</button>
+			    <button id="modal-btn-accept" type="button" class="btn btn-primary">${
+					modalOptions.acceptText
+				}</button>
 			  </div>
 		    </div>
 		  </div>
 		`;
 
-		$( "body" ).append( modalElem );
-		var myModal = $( modalElem ).modal( "show" );
+		$("body").append(modalElem);
+		var myModal = $(modalElem).modal("show");
 
-		$( "#modal-btn-cancel,#modal-btn-accept", myModal ).on(
+		$("#modal-btn-cancel,#modal-btn-accept", myModal).on(
 			"click",
-			function( e ){
-				myModal.modal( "hide" );
-				$( modalElem ).remove();
-				return (e.target.id == 'modal-btn-accept') ? resolve() : false;
+			function (e) {
+				myModal.modal("hide");
+				$(modalElem).remove();
+				return e.target.id == "modal-btn-accept" ? resolve() : false;
 			}
 		);
 	});
-})
-
+});
