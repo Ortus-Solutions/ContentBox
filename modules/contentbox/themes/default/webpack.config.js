6fc99a895ca9416fc7630c6914f0da02dd821721
Luis Majano <lmajano@gmail.com>
2023-08-08 15:33:55 +0200

    code formatting [no ci]

--- modules/contentbox/themes/default/webpack.config.js
+++ modules/contentbox/themes/default/webpack.config.js
@@ -1,38 +1,37 @@
-const elixir = require("coldbox-elixir");
+const elixir = require( "coldbox-elixir" );
 const fs = require( "fs-extra" );
 const path = require( "path" );
-const webpack = require("webpack");
+const webpack = require( "webpack" );
 
-module.exports = elixir(function(mix) {
-	elixir.config.mergeConfig({
-        module: {
-            // Globally exposed variables
-            rules: [
-                {
-                    test: require.resolve("jquery"),
-                    loader: "expose-loader",
-                    options: {
-                      exposes: ["$", "jQuery"],
-                    },
-                }
-            ]
-        },
-        resolve: {
-            alias: {
-              jQuery : "jquery"
-            }
-        },
-        plugins: [
-            // Variables only resolved/exposed globally within the compiled JS assets
-            new webpack.ProvidePlugin( {
-                $              : "jquery",
-                jquery         : "jquery",
-                "window.jQuery": "jquery",
-                "window.$": "jquery",
-                jQuery         :"jquery"
-            } )
+module.exports = elixir( function( mix ) {
+	elixir.config.mergeConfig( {
+		module : {
+			// Globally exposed variables
+			rules : [
+				{
+					test    : require.resolve( "jquery" ),
+					loader  : "expose-loader",
+					options : {
+						exposes : [
+							"$",
+							"jQuery"
+						],
+					},
+				}
+			]
+		},
+		resolve : { alias: { jQuery: "jquery" } },
+		plugins : [
+			// Variables only resolved/exposed globally within the compiled JS assets
+			new webpack.ProvidePlugin( {
+				$               : "jquery",
+				jquery          : "jquery",
+				"window.jQuery" : "jquery",
+				"window.$"      : "jquery",
+				jQuery          : "jquery"
+			} )
 		]
-	});
+	} );
 
 	var bootswatchDir = "node_modules/bootswatch";
 	var swatchesDir = "includes/bootswatch";
@@ -41,10 +40,10 @@ module.exports = elixir(function(mix) {
 	mix.js( "theme.js" );
 
 	fs.emptyDirSync( swatchesDir );
-	fs.readdirSync(bootswatchDir).forEach( element => {
-		if( element.indexOf( "." ) === -1 ){
-			fs.copySync( path.join(bootswatchDir, element ), path.join( swatchesDir, element ), { overwrite : true } );
+	fs.readdirSync( bootswatchDir ).forEach( element => {
+		if ( element.indexOf( "." ) === -1 ){
+			fs.copySync( path.join( bootswatchDir, element ), path.join( swatchesDir, element ), { overwrite: true } );
 		}
-    });
-	fs.copySync( path.join( swatchesDir, 'flatly' ), path.join( swatchesDir, 'green' ) );
-});
+	} );
+	fs.copySync( path.join( swatchesDir, "flatly" ), path.join( swatchesDir, "green" ) );
+} );
